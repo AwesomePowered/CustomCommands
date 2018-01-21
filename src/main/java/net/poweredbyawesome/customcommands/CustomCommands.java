ec37d58902b009669f52392e602b66880a5750b4
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2018-01-21 01:05:11 -0800

    A few format changes/placeholders

--- src/main/java/net/poweredbyawesome/customcommands/CustomCommands.java
+++ src/main/java/net/poweredbyawesome/customcommands/CustomCommands.java
@@ -1,7 +1,9 @@
 package net.poweredbyawesome.customcommands;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -32,8 +34,33 @@ public final class CustomCommands extends JavaPlugin implements Listener {
         if (cmds.contains(command)) {
             ev.setCancelled(true);
             for (String s : getConfig().getStringList(command)) {
-                ev.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s));
+                if (s.startsWith("/") || s.startsWith("\\")) {
+                    if (s.startsWith("/")) {
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), format(s.replace("/", ""), ev.getPlayer()));
+                    }
+                    if (s.startsWith("\\")) {
+                        ev.getPlayer().performCommand(format(s.replace("\\", ""), ev.getPlayer()));
+                    }
+                } else {
+                    if (s.contains("<*>")) {
+                        String multiplier = s.split("<*>")[1];
+                        if (StringUtils.isNumeric(multiplier)) {
+                            for (int i = 0; i < Integer.valueOf(s.split("<*>")[1]); i++) {
+                                ev.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', format(s.replace("<*>","").replace(multiplier, ""), ev.getPlayer())));
+                            }
+                        }
+                    } else {
+                        ev.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', format(s, ev.getPlayer())));
+                    }
+                }
             }
         }
+    }
+
+    public String format(String message, Player player) {
+        message = message.replace("<player>", player.getName());
+        message = message.replace("<health>", String.valueOf(player.getHealth()));
+        message = message.replace("<hunger>", String.valueOf(player.getFoodLevel()));
+        return message;
     }
 }
